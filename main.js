74ad5b990654d207069f7126309657216ae3d5a9
hemshah011 <hemshah011@gmail.com>
2021-05-27 09:56:23 +0530

    animations

--- main.js
+++ main.js
@@ -20,3 +20,20 @@ const updateCountdown = () => {
 }
 
 setInterval(updateCountdown,1000);
+
+const createSnowFlake = () => {
+	const snow_flake = document.createElement('i');
+	snow_flake.classList.add('fas');
+	snow_flake.classList.add('fa-snowflake');
+	snow_flake.style.left = Math.random() * window.innerWidth + 'px';
+	snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's'; 
+	snow_flake.style.opacity = Math.random();
+	snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
+	document.body.appendChild(snow_flake);
+	setTimeout(() => {
+		snow_flake.remove();
+	}, 5000)
+}
+
+setInterval(updateCountdown, 1000);
+setInterval(createSnowFlake, 50);
